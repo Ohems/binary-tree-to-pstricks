a94a7cd546e5f94aa5bc1d873945fc2f2f127128
Ohems <finnboy11@hotmail.com>
2017-05-03 00:48:31 +0300

    Add comments to binary tree class

--- BinaryTreeToPSTricks/binarytree.cpp
+++ BinaryTreeToPSTricks/binarytree.cpp
@@ -37,32 +37,43 @@ namespace {
         Node* left = originalLeft;
 
         while(right && left && right != left) {
+            // Account for the offset of the current subtrees
             rightMod += right->mod();
             leftMod += left->mod();
 
-            float diff = left->x() + leftMod + nodeWidth(left) + margin()
+            // Calculate the overlay of the current nodes
+            float overlay = left->x() + leftMod + nodeWidth(left) + margin()
                     - (right->x() + rightMod);
 
-            if (diff > error) {
-                error = diff;
+            // If overlay is greater than the largest current error, replace
+            if (overlay > error) {
+                error = overlay;
             }
 
             float rightThreadMod;
             float leftThreadMod;
 
+            // Progress to the next nodes
+            // If we jump over a thread, record the mod offset
             right = right->leftContour(&rightThreadMod);
             left = left->rightContour(&leftThreadMod);
 
+            // Apply thread mod offset to the known mods
             rightMod += rightThreadMod;
             leftMod += leftThreadMod;
 
+            // Check right tree ended but the left one is still going
             if (!right && left) {
+                // Create a new thread from right to left
                 originalRight->rightLast()->thread(
                             left,
                             leftMod - (rightMod + error)
                 );
             }
+
+            // Check left tree ended but the right one is still going
             if (!left && right) {
+                // Create a new thread from left to right
                 originalLeft->leftLast()->thread(
                             right,
                             (rightMod + error) - leftMod
@@ -101,6 +112,7 @@ void BinaryTree::placeNodesRecursive(
         current->mod() += findGreatestErrorInContour(current, sibling);
     }
 
+    // Keep track of the layer count
     if (current->depth() + 1 > layers_) {
         layers_ = current->depth() + 1;
     }
@@ -108,8 +120,10 @@ void BinaryTree::placeNodesRecursive(
 
 void BinaryTree::applyModRecursive(Node* current, float mod /*= 0.0f*/)
 {
+    // Record the offset of this subtree
     mod += current->mod();
 
+    // Convert mod into location
     current->x() += mod;
     current->mod() = 0;
 
@@ -119,6 +133,7 @@ void BinaryTree::applyModRecursive(Node* current, float mod /*= 0.0f*/)
         width_ = rightEdge;
     }
 
+    // Recurse
     for (Node* child : current->children()) {
         applyModRecursive(child, mod);
     }
@@ -126,10 +141,9 @@ void BinaryTree::applyModRecursive(Node* current, float mod /*= 0.0f*/)
 
 BinaryTree::BinaryTree(json j)
 {
-    buildNodesRecursive(root_, j);
-
-    placeNodesRecursive(root_);
-    applyModRecursive(root_);
+    buildNodesRecursive(root_, j);  // Build tree
+    placeNodesRecursive(root_);     // Calculate tree locations
+    applyModRecursive(root_);       // Apply calculated locations
 }
 
 BinaryTree::~BinaryTree()
